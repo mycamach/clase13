d508b573562122469f2f27eb146c509467e800f8
mycamach <manoli.camacho.a@gmail.com>
2017-08-28 19:44:42 -0300

    1 ready

---
+++ ejercicio1.rb
@@ -0,0 +1,9 @@
+=begin
+1. Dado el siguiente hash:
+h = { :claveuno => 10, :clavedos => 20, :clavetres => 30 }
+Modificar el hash para utilizar la sintaxis de Ruby 2.0+
+=end
+
+h = {claveuno: 10, clavedos: 20, clavetres: 30 }
+
+print h
